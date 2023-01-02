0b54f54b03ef31fca854fbfeaa3f0efdbc2c074d
Andrew Pacheco <ndrewpacheco@gmail.com>
2023-01-02 01:07:37 -0500

    fin exercise

--- partone.js
+++ partone.js
@@ -1,20 +1,44 @@
 const body = document.querySelector("body");
-let favNumber = 12;
-body.innerText = `Your fav number is: ${favNumber}\n`;
+const button = document.querySelector("button");
+let deckId;
+axios
+  .get("https://deckofcardsapi.com/api/deck/new/")
+  .then((res) => (deckId = res.data.deck_id));
 
-function getNumberData(url) {
-  axios.get(url).then((res) => {
-    body.innerText += res.data.text + "\n";
-  });
+function drawCard() {
+  axios
+    .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    .then((res) => {
+      res.data.cards[0];
+      const img = document.createElement("img");
+      img.src = res.data.cards[0].image;
+      body.appendChild(img);
+    })
+    .catch(() => body.removeEventListener("click", drawCard));
 }
 
-getNumberData(`http://numbersapi.com/${favNumber}?json`);
-getNumberData(`http://numbersapi.com/${favNumber}?json`);
-getNumberData(`http://numbersapi.com/${favNumber}?json`);
-getNumberData(`http://numbersapi.com/${favNumber}?json`);
+body.addEventListener("click", drawCard);
+// axios
+//   .get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
+//   .then((res) => {
+//     deckId = res.data.deck_id;
+//     console.log(res.data);
 
-// axios.get("http://numbersapi.com/1..3,10").then((res) => {
-//   for (let num in res.data) {
-//     body.innerHTML += "<div>" + res.data[num] + "<div>";
-//   }
-// });
+//     console.log(res.data.cards[0].code);
+//     const img = document.createElement("img");
+//     img.src = res.data.cards[0].image;
+
+//     body.appendChild(img);
+//   })
+//   .then(() => {
+//     axios
+//       .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+//       .then((res) => {
+//         console.log(res.data);
+//         console.log(res.data.cards[0].code);
+//         const img = document.createElement("img");
+//         img.src = res.data.cards[0].image;
+
+//         body.appendChild(img);
+//       });
+//   });
