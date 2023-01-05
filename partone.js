a81dafd0da5b49bbe740ea70444fa9479e9624a7
Andrew Pacheco <ndrewpacheco@gmail.com>
2023-01-05 11:55:03 -0500

    workingon ex 3

--- partone.js
+++ partone.js
@@ -1,44 +1,56 @@
 const body = document.querySelector("body");
-const button = document.querySelector("button");
 let deckId;
+
+// part 1
+counter = 0;
+function appendCard(res) {
+  let deckId = res.data.deck_id;
+
+  const img = document.createElement("img");
+  img.src = res.data.cards[0].image;
+  body.appendChild(img);
+}
+
+// async function getTwoCards() {
+//   let res = await axios.get(
+//     "https://deckofcardsapi.com/api/deck/new/draw/?count=1"
+//   );
+//   appendCard(res);
+
+//   res = await axios.get(
+//     `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+//   );
+//   appendCard(res);
+// }
+
+// getTwoCards();
+
+// part 2
+
 axios
-  .get("https://deckofcardsapi.com/api/deck/new/")
+  .get("https://deckofcardsapi.com/api/deck/new/shuffle")
   .then((res) => (deckId = res.data.deck_id));
 
-function drawCard() {
-  axios
-    .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    .then((res) => {
-      res.data.cards[0];
-      const img = document.createElement("img");
-      img.src = res.data.cards[0].image;
-      body.appendChild(img);
-    })
-    .catch(() => body.removeEventListener("click", drawCard));
+async function drawCard() {
+  try {
+    let res = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    );
+    appendCard(res);
+    counter += 1;
+    console.log("counter is: " + counter);
+  } catch (e) {
+    body.removeEventListener("click", drawCard);
+  }
+
+  // .then((res) => {
+  //   res.data.cards[0];
+  //   const img = document.createElement("img");
+  //   img.src = res.data.cards[0].image;
+  //   body.appendChild(img);
+  // })
+
+  // .catch(() => body.removeEventListener("click", drawCard));
 }
 
 body.addEventListener("click", drawCard);
-// axios
-//   .get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
-//   .then((res) => {
-//     deckId = res.data.deck_id;
-//     console.log(res.data);
-
-//     console.log(res.data.cards[0].code);
-//     const img = document.createElement("img");
-//     img.src = res.data.cards[0].image;
-
-//     body.appendChild(img);
-//   })
-//   .then(() => {
-//     axios
-//       .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-//       .then((res) => {
-//         console.log(res.data);
-//         console.log(res.data.cards[0].code);
-//         const img = document.createElement("img");
-//         img.src = res.data.cards[0].image;
-
-//         body.appendChild(img);
-//       });
-//   });
